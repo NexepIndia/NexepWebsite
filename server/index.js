cf10ee629efab9edab93b6608b9ed909a3e0ec06
Anup Kumar <your_github_rachitjha999@gmail.com>
2026-04-12 13:38:02 +0530

    adding

--- server/index.js
+++ server/index.js
@@ -55,11 +55,7 @@ app.post('/send-email', async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
